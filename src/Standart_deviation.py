bec12b23ad6b0edc087d24f5d40532c466fbced5
Keligao007 <keligao007@gmail.com>
2023-04-16 23:27:36 +0200

    error fix

--- src/Standart_deviation.py
+++ src/Standart_deviation.py
@@ -12,7 +12,6 @@ numbers = [float(number) for number in numbers]
 
 ############################# mean if numbers
 
-
 lenght_of_numbers_list = len(numbers)
 
 sum_of_numbers = 0
@@ -23,12 +22,14 @@ mean_of_numbers =  div(sum_of_numbers, lenght_of_numbers_list)
 print ("mean of numbers is", mean_of_numbers)
 
 ############################# standart deviation
-
+##### sum (xi- x_priemer)pwr 2
 sum_num_sub_mean = 0
-for num in numbers:
-       sum_num_sub_mean = add(sum_num_sub_mean, pwr(sub(num, mean_of_numbers), 2))
-       
+for x in numbers: 
+       sum_num_sub_mean = add(sum_num_sub_mean, pwr(sub(x, mean_of_numbers), 2))
 
-Standart_deviation = sqrt(div(sum_num_sub_mean, sub(lenght_of_numbers_list, 1)))
+##### N - 1 
+N_sub_1 = sub(lenght_of_numbers_list, 1)    
+
+Standart_deviation = sqrt(div(sum_num_sub_mean, N_sub_1))
 
 print ("Standart deviation of the numbers is ", Standart_deviation)
