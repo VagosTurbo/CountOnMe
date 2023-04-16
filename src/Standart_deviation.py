87e892d4dad62ecaa85f577984e28bed898aa48e
Keligao007 <keligao007@gmail.com>
2023-04-16 20:59:11 +0200

    Standartna odchylka este treba oprava

---
+++ src/Standart_deviation.py
@@ -0,0 +1,35 @@
+from mathlib import *
+
+input_numbers = input()
+
+numbers = input_numbers.split()
+
+if len(numbers) < 2:
+    print("nizky pocet cisiel")
+    exit()
+
+numbers = [float(number) for number in numbers]
+
+############################# mean if numbers
+
+lenght_of_numbers_list = 0
+for num in numbers:
+        lenght_of_numbers_list = add(lenght_of_numbers_list, 1)
+
+sum_of_numbers = 0
+for num in numbers:
+        sum_of_numbers = add(sum_of_numbers, num)
+
+mean_of_numbers =  div(sum_of_numbers, lenght_of_numbers_list)
+print ("mean of numbers is", mean_of_numbers)
+
+############################# standart deviation
+
+sum_num_sub_mean = 0
+for num in numbers:
+       sum_num_sub_mean = add(sum_num_sub_mean, pwr(sub(num, mean_of_numbers), 2))
+       
+
+Standart_deviation = sqrt(div(sum_num_sub_mean, sub(lenght_of_numbers_list, 1)))
+
+print ("Standart deviation of the numbers is ", Standart_deviation)
